5efd573f6802809dd209b561e16ffae78ce71972
chadsfather <chadsfather@gmail.com>
2020-01-31 08:30:06 -0500

    On branch fix_dragevent chequea los cambios Changes to be committed: 	modified:   src/app/index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,53 +1,53 @@
 require('./index.scss');
 
-
 function draggCards(){
-    const fill = document.querySelector('.card');
+    const fill = document.querySelectorAll('.card');
     const empties = document.querySelectorAll('.empty-boxes');
 
     // Fill listeners
-    fill.addEventListener('dragstart', dragStart);
-    fill.addEventListener('dragend', dragEnd);
+    fill.forEach((item) => {
+        item.addEventListener('dragstart', dragStart);
+        item.addEventListener('dragend', dragEnd);
+    })
 
     // Loop through empty boxes and add listeners
     for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
+        empty.addEventListener('dragover', dragOver);
+        empty.addEventListener('dragenter', dragEnter);
+        empty.addEventListener('dragleave', dragLeave);
+        empty.addEventListener('drop', dragDrop);
     }
 
     // Drag Functions
 
     function dragStart() {
-    this.className += ' hold';
-    setTimeout(() => (this.className = 'invisible'), 0);
+        this.className += ' hold';
+        setTimeout(() => (this.className = 'invisible'), 0);
     }
 
     function dragEnd() {
-    this.className = 'card';
+        this.className = 'card';
     }
 
     function dragOver(e) {
-    e.preventDefault();
+        e.preventDefault();
     }
 
     function dragEnter(e) {
-    e.preventDefault();
-    this.className += ' hovered';
+        e.preventDefault();
+        this.className += ' hovered';
     }
 
     function dragLeave() {
-    this.className = 'empty-boxes';
+        this.className = 'empty-boxes';
     }
 
     function dragDrop() {
-    this.className = 'empty-boxes';
-    this.append(fill);
+        this.className = 'empty-boxes';
+        this.append(fill);
     }
 
 }
-
 
 document.addEventListener('DOMContentLoaded', draggCards);
 
